0459376eef8cb68f2cd685d08780020ca21e5fbd
Prabhu Gps <prabhugsp@collab.net>
2012-06-20 16:21:02 +0000

    [artf7253] Integrate TeamForge 6.2 code
    * console/svn-server/lib/viewvc/viewvc.py
      we need 'self.roottype' populated here itself since we are using it 
      in our teamforge authorizer's constructor.
    
    * console/grails-app/services/com/collabnet/svnedge/admin/ServerConfService.groovy
      (writeSvnViewvcConf): Load mod_authnz_ctf.so
      (getCtfSvnHttpdConf): Removed all sfauth related conf and include
                            mod_authnz_ctf related conf.
    
    * integration/ctf/installer/viewvc/lib/vcauth/teamforge/__init__.py
      Use suds to get the role paths and authorize them.
      (check_universal_access): check if the associated user is allowed to read
                                in all paths in rootname.
      Integrating the viewvc code required to authorize againt ctfauthorizer.so
    
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/svnedge/trunk@3193 03e8f217-bfc6-4b7c-bcb7-0738c91e2c5f

--- ctf/installer/viewvc/lib/vcauth/teamforge/__init__.py
+++ ctf/installer/viewvc/lib/vcauth/teamforge/__init__.py
@@ -11,6 +11,9 @@ import string
 import SourceForge
 import vcauth
 import vclib
+import ctfauthorizer
+
+from suds.client import Client
 
 class ViewVCAuthorizer(vcauth.GenericViewVCAuthorizer):
   """Custom ViewVC authorization module for TeamForge."""
@@ -25,69 +28,92 @@ class ViewVCAuthorizer(vcauth.GenericViewVCAuthorizer):
     self.perm_cache = { } # A cache of the previously authorized paths
     self.roottype = params['roottype'] # The type of repository being browsed ('cvs' or 'svn')
     self.query_dict = params['query_dict'] # The query parameters
-    self.global_access = None # Simple boolean to short-circuit authorizations
+
+    # A single-item array, where the item is a universal access
+    # determination (1=yes, 0=no, None=dunno); or None if we haven't
+    # queried for universal access yet.
+    self.global_access = None
+
+    system_id = self.query_dict['system']
+
+    if self.roottype == "svn":
+        SOAPServiceUrl = SourceForge.getSOAPServiceUrl("ScmListener")
+
+        # add proxy settings if they exist
+        httpProxy = SourceForge.getProxyUrl()
+        proxyDict = {}
+        if httpProxy and len(httpProxy) > 0:
+            proxyDict[SourceForge.getProxyProtocol(SOAPServiceUrl)] = httpProxy
+
+        # Create the client with proxy and location options    
+        # We need to override the location from the WSDL which reports localhost
+
+        scm = Client(SOAPServiceUrl + '?wsdl', location=SOAPServiceUrl, proxy=proxyDict)
+
+        key = SourceForge.createScmRequestKey()
+        # if usename is empty then set as 'nobody' user
+        if username is None or username == '':
+            username = 'nobody'
+        raw_pbps = scm.service.getRolePaths(key, username, system_id, self.query_dict['root'])
+        raw_pbps = map(str, raw_pbps)
+        ctfauthorizer.set_pbps(raw_pbps)
 
   def check_root_access(self, rootname):
     """Return 1 if the associated username is permitted to read ROOTNAME."""
     return 1
 
-  def check_global_access(self, rootname):
-    """Return 1 if the associated username is permitted to read ALL paths in ROOTNAME."""
-    # Quick return for CVS repositories
+  def check_universal_access(self, rootname):
+    """Return 1 if the associated username is permitted to read ALL
+    paths in ROOTNAME, 0 if username is permitted to read NO paths in
+    ROOTNAME, or None if no such universal access determination can be
+    made."""
+
+    if self.global_access is not None:
+      return self.global_access[0]
+    
     if self.roottype == "cvs":
-      self.global_access = 1
-      return self.global_access
-
-    if self.global_access is None:
-      # Call SourceForge.load() manually if a custom sourceforge.properties path is used
-      if os.environ.has_key('SOURCEFORGE_PROPERTIES_PATH'):
-        SourceForge.load(filename=os.environ['SOURCEFORGE_PROPERTIES_PATH'])
-
+      self.global_access = [1]
+    else:
       # Check for global view access
-      path, all, any = SourceForge.getScmPermissionForPath(self.username,
-                                                             self.query_dict['system'],
-                                                             rootname + '/',
-                                                             'view')
-
-      if all == 0:
-        self.global_access = 1
+      pbp_access = ctfauthorizer.determine_global_access()
+      if pbp_access:
+        self.global_access = [1]
       else:
-        self.global_access = 0
-
-    return self.global_access
+        self.global_access = [None]
+    return self.global_access[0]
 
   def check_path_access(self, rootname, path_parts, pathtype, rev=None):
     """Return 1 if the associated username is permitted to read
     revision REV of the path PATH_PARTS (of type PATHTYPE) in
     repository ROOTNAME."""
+
     has_access = 0
     repo_path = '%s/%s' % (rootname, string.join(path_parts, '/'))
+    path_inside_repo = '/%s' % (string.join(path_parts, '/'))
 
     # Short circuit the authorization check when the user is known to have
-    # global access.
-    if self.check_global_access(rootname):
-      return 1
+    # a universal access determination (either granting or denying access).
+    univ_access = self.check_universal_access(rootname)
+    if univ_access is not None:
+      return univ_access
 
     # If we have a cached permission for this path, use it
     if self.perm_cache.has_key(repo_path):
       return self.perm_cache[repo_path]
-
-    # No cache, lets get the permission by querying TeamForge
-    path, all, any = SourceForge.getScmPermissionForPath(self.username,
-                                                         self.query_dict['system'],
-                                                         repo_path,
-                                                         'view')
-
-    # Take the servlet response and convert to a ViewVC response
-    # FIXME: Using the any response is a way to let ViewVC show intermediary
-    # directory paths to get access to descendants that you do have access to
-    # when you might not necessary have access to the intermediary.  That being
-    # said, you can potentially leak revision information under certain
-    # situations.  This is being tracked in artf76368.
-    if any != 0:
-      has_access = 0
-    else:
+    
+    # The root of the repository should always be readable.
+    # For example, 
+    #   PBP: 
+    #    /: None
+    #    /trunk: view
+    #    /branches: view
+    # Even though the user doesn't have access to the root of the repository,
+    # all other paths which he/she has access should be viewable.      
+    if path_inside_repo == "/":
       has_access = 1
+    else:
+      # No cache, lets get the permission using our authorizer.
+      has_access = ctfauthorizer.has_non_recursive_access(path_inside_repo, 1)
 
     self.perm_cache[repo_path] = has_access
 
